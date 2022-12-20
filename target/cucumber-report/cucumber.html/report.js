58284a30594013a4a2ddfd268a20c15b828b8391
Savitrialagiya <savunilesh@gmail.com>
2022-12-20 17:23:34 +0000

    cucumber-nopecommerce1-week6-hw

---
+++ target/cucumber-report/cucumber.html/report.js
@@ -0,0 +1,623 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("electronicspage.feature");
+formatter.feature({
+  "line": 1,
+  "name": "Electronics Page Test",
+  "description": "As a user I would like to go to electronics page in nop commerce website",
+  "id": "electronics-page-test",
+  "keyword": "Feature"
+});
+formatter.before({
+  "duration": 4767342900,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 14,
+  "name": "Verify That The Product Added Successfully And Place Order Successfully",
+  "description": "",
+  "id": "electronics-page-test;verify-that-the-product-added-successfully-and-place-order-successfully",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 12,
+      "name": "@sanity1"
+    },
+    {
+      "line": 13,
+      "name": "@regression"
+    }
+  ]
+});
+formatter.step({
+  "line": 15,
+  "name": "I am on Homepage",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 16,
+  "name": "I mouseHover On Electronic Tab",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 17,
+  "name": "I click On Cell Phones",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 18,
+  "name": "I verify cell phone text",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 19,
+  "name": "I click On List View Tab",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 20,
+  "name": "I click On Nokia Lumia Product",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 21,
+  "name": "I verify Nokia Lumia Text",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 22,
+  "name": "I verify Price",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 23,
+  "name": "I change The Quantity",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 24,
+  "name": "I click On Add To Cart Buttons",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 25,
+  "name": "I verify Product Added In Shopping Cart Text",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 26,
+  "name": "I click On Close Button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 27,
+  "name": "I hover On Shopping Cart And Click On Go To Cart Button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 28,
+  "name": "I verify Shopping Cart Text",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 29,
+  "name": "I verify The Quantity Is two",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 30,
+  "name": "I verify The Total",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 31,
+  "name": "I click On Check Box",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 32,
+  "name": "I click On Check out Button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 33,
+  "name": "I verify Welcome Message",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 34,
+  "name": "I click On Register Tab",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 35,
+  "name": "I verify Register Text",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 36,
+  "name": "I enter text in mandatory Fields",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 37,
+  "name": "I click On Registration Button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 38,
+  "name": "I verify Registration Text",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 39,
+  "name": "I click On Continue Tab",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 40,
+  "name": "I check Shopping Cart Text",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 41,
+  "name": "I click on Login",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 42,
+  "name": "I enter userName",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 43,
+  "name": "I enter password",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 44,
+  "name": "I click on login button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 45,
+  "name": "I click On Terms And Services Box",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 46,
+  "name": "I click On Checkout Last",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 47,
+  "name": "I fill Delivery Address Fields",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 48,
+  "name": "I click On Continues Button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 49,
+  "name": "I click On second Day Air Button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 50,
+  "name": "I click Continue",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 51,
+  "name": "I select Credit Card Radio Button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 52,
+  "name": "I click Con",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 53,
+  "name": "I select Visa From DropDown",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 54,
+  "name": "I fill Card Details",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 55,
+  "name": "I click Con Btn",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 56,
+  "name": "I verify Credit Card Text",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 57,
+  "name": "I verify second Day Air Text",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 58,
+  "name": "I verify Final Total",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 59,
+  "name": "I click On Confirm Button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 60,
+  "name": "I verify Thank You Text",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 61,
+  "name": "I verify Successfully Processed Message",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 62,
+  "name": "I click On Continue",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 63,
+  "name": "I verify Welcome To Our Store Text",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "ComputerSteps.iAmOnHomepage()"
+});
+formatter.result({
+  "duration": 169064500,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ElectronicsSteps.iMouseHoverOnElectronicTab()"
+});
+formatter.result({
+  "duration": 237167700,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ElectronicsSteps.iClickOnCellPhones()"
+});
+formatter.result({
+  "duration": 1089895000,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ElectronicsSteps.iVerifyCellPhoneText()"
+});
+formatter.result({
+  "duration": 77405600,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ElectronicsSteps.iClickOnListViewTab()"
+});
+formatter.result({
+  "duration": 112883500,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ElectronicsSteps.iClickOnNokiaLumiaProduct()"
+});
+formatter.result({
+  "duration": 2898899600,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ElectronicsSteps.iVerifyNokiaLumiaText()"
+});
+formatter.result({
+  "duration": 74653300,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ElectronicsSteps.iVerifyPrice()"
+});
+formatter.result({
+  "duration": 67058300,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ElectronicsSteps.iChangeTheQuantity()"
+});
+formatter.result({
+  "duration": 186499700,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ElectronicsSteps.iClickOnAddToCartButtons()"
+});
+formatter.result({
+  "duration": 94939900,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ElectronicsSteps.iVerifyProductAddedInShoppingCartText()"
+});
+formatter.result({
+  "duration": 339508600,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ElectronicsSteps.iClickOnCloseButton()"
+});
+formatter.result({
+  "duration": 116273900,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ElectronicsSteps.iHoverOnShoppingCartAndClickOnGoToCartButton()"
+});
+formatter.result({
+  "duration": 1466017100,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ElectronicsSteps.iVerifyShoppingCartText()"
+});
+formatter.result({
+  "duration": 90906100,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ElectronicsSteps.iVerifyTheQuantityIsTwo()"
+});
+formatter.result({
+  "duration": 86238800,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ElectronicsSteps.iVerifyTheTotal()"
+});
+formatter.result({
+  "duration": 89017300,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ElectronicsSteps.iClickOnCheckBox()"
+});
+formatter.result({
+  "duration": 127903900,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ElectronicsSteps.iClickOnCheckOutButton()"
+});
+formatter.result({
+  "duration": 757791000,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ElectronicsSteps.iVerifyWelcomeMessage()"
+});
+formatter.result({
+  "duration": 82368200,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ElectronicsSteps.iClickOnRegisterTab()"
+});
+formatter.result({
+  "duration": 413621200,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ElectronicsSteps.iVerifyRegisterText()"
+});
+formatter.result({
+  "duration": 81043200,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ElectronicsSteps.iEnterTextInMandatoryFields()"
+});
+formatter.result({
+  "duration": 1291103900,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ElectronicsSteps.iClickOnRegistrationButton()"
+});
+formatter.result({
+  "duration": 724936600,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ElectronicsSteps.iVerifyRegistrationText()"
+});
+formatter.result({
+  "duration": 77028400,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ElectronicsSteps.iClickOnContinueTab()"
+});
+formatter.result({
+  "duration": 453416200,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ElectronicsSteps.iCheckShoppingCartText()"
+});
+formatter.result({
+  "duration": 84029400,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ElectronicsSteps.iClickOnLogin()"
+});
+formatter.result({
+  "duration": 509302500,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ElectronicsSteps.iEnterUserName()"
+});
+formatter.result({
+  "duration": 139565500,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ElectronicsSteps.iEnterPassword()"
+});
+formatter.result({
+  "duration": 146718900,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ElectronicsSteps.iClickOnLoginButton()"
+});
+formatter.result({
+  "duration": 776449100,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ElectronicsSteps.iClickOnTermsAndServicesBox()"
+});
+formatter.result({
+  "duration": 103110500,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ElectronicsSteps.iClickOnCheckoutLast()"
+});
+formatter.result({
+  "duration": 1130901800,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ElectronicsSteps.iFillDeliveryAddressFields()"
+});
+formatter.result({
+  "duration": 717216300,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ElectronicsSteps.iClickOnContinuesButton()"
+});
+formatter.result({
+  "duration": 70483500,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ElectronicsSteps.iClickOnSecondDayAirButton()"
+});
+formatter.result({
+  "duration": 330454700,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ElectronicsSteps.iClickContinue()"
+});
+formatter.result({
+  "duration": 72608900,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ElectronicsSteps.iSelectCreditCardRadioButton()"
+});
+formatter.result({
+  "duration": 373418300,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ElectronicsSteps.iClickCon()"
+});
+formatter.result({
+  "duration": 103847800,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ElectronicsSteps.iSelectVisaFromDropDown()"
+});
+formatter.result({
+  "duration": 354788200,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ElectronicsSteps.iFillCardDetails()"
+});
+formatter.result({
+  "duration": 762597100,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ElectronicsSteps.iClickConBtn()"
+});
+formatter.result({
+  "duration": 131054400,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ElectronicsSteps.iVerifyCreditCardText()"
+});
+formatter.result({
+  "duration": 453152100,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ElectronicsSteps.iVerifySecondDayAirText()"
+});
+formatter.result({
+  "duration": 92014100,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ElectronicsSteps.iVerifyFinalTotal()"
+});
+formatter.result({
+  "duration": 64304100,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ComputerSteps.iClickOnConfirmButton()"
+});
+formatter.result({
+  "duration": 78248900,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ComputerSteps.iVerifyThankYouText()"
+});
+formatter.result({
+  "duration": 776825500,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ElectronicsSteps.iVerifySuccessfullyProcessedMessage()"
+});
+formatter.result({
+  "duration": 46601600,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ElectronicsSteps.iClickOnContinue()"
+});
+formatter.result({
+  "duration": 447823300,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ElectronicsSteps.iVerifyWelcomeToOurStoreText()"
+});
+formatter.result({
+  "duration": 57736000,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 350100,
+  "status": "passed"
+});
+});
